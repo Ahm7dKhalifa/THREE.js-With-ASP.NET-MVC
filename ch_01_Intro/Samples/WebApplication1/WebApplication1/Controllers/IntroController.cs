f0fc19e8a485c68718e1e214b02ab26513195fe5
ahm7dkhalifa <ahm7dkhalifa@gmail.com>
2018-09-05 13:04:18 +0300

    ADD ch_01_Intro

---
+++ ch_01_Intro/Samples/WebApplication1/WebApplication1/Controllers/IntroController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class IntroController : Controller
+    {
+        // GET: Intro
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SecondExample()
+        {
+            return View();
+        }
+
+        public ActionResult DrawLine()
+        {
+            return View();
+        }
+    }
+}
