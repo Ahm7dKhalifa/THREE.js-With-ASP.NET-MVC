ebae5b2fe1a668b6d225969b79bb47ed823acae1
ahm7dkhalifa <ahm7dkhalifa@gmail.com>
2018-09-05 13:05:36 +0300

    ADD ch_02_Scene_and_SkyBox_and_Plane

---
+++ ch_02_Scene_and_SkyBox_and_Plane/Samples/SceneApp/SceneApp/Controllers/SceneController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SceneApp.Controllers
+{
+    public class SceneController : Controller
+    {
+        // GET: Scene
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SceneWithGUI()
+        {
+            return View();
+        }
+
+        public ActionResult example2()
+        {
+            return View();
+        }
+
+        public ActionResult OneTextureForAllFaces()
+        {
+            return View();
+        }
+
+        public ActionResult EveryTextureForEveryFace()
+        {
+            return View();
+        }
+        public ActionResult OneTexture()
+        {
+            return View();
+        }
+        public ActionResult CubeSkyBox()
+        {
+            return View();
+        }
+
+        public ActionResult CubeSkyBox2()
+        {
+            return View();
+        }
+
+        public ActionResult CubeSkyBox3()
+        {
+            return View();
+        }
+        public ActionResult CubeSkyBox4()
+        {
+            return View();
+        }
+        public ActionResult CubeSkyBoxYokahama()
+        {
+            return View();
+        }
+        public ActionResult SphereSkyBox360()
+        {
+            return View();
+        }
+        public ActionResult SphereSkyBox360_2()
+        {
+            return View();
+        }
+        public ActionResult SphereSkyBoxPanorama()
+        {
+            return View();
+        }
+
+        public ActionResult CubeMapWithShader()
+        {
+            return View();
+        }
+
+        public ActionResult CubeMapWithShader2()
+        {
+            return View();
+        }
+        public ActionResult CubeMapWithShaderYokahama()
+        {
+            return View();
+        }
+        public ActionResult NormalCubeWithOutShader()
+        {
+            return View();
+        }
+    }
+}
