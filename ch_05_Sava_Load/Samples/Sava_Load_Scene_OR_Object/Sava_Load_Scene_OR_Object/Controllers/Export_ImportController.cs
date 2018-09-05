be05a4423876fd1e0b79082d06b7bd32a4f1e1f0
ahm7dkhalifa <ahm7dkhalifa@gmail.com>
2018-09-05 13:09:25 +0300

    ADD ch_05_Sava_Load

---
+++ ch_05_Sava_Load/Samples/Sava_Load_Scene_OR_Object/Sava_Load_Scene_OR_Object/Controllers/Export_ImportController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//working with file
+using System.IO;
+namespace Sava_Load_Scene_OR_Object.Controllers
+{
+    public class Export_ImportController : Controller
+    {
+        /************************************************************************
+         * **********************************************************************
+         *  1. working with  json file
+         *  **********************************************************************
+         *  ********************************************************************/
+        /*#######################################################
+         * save scene
+         *#######################################################*/
+        // Download_Scene_As_JSON_File
+        public ActionResult Download_Scene_As_JSON_File()
+        {
+            return View();
+        }
+        // Save_Scene_As_JSON_File_IN_Server
+        public ActionResult Save_Scene_As_JSON_File_IN_Server()
+        {
+            ViewBag.Succ = null;
+            return View();
+        }
+        // Save_Scene_As_JSON_File_IN_Server
+        [HttpPost]
+        public ActionResult Save_Scene_As_JSON_File_IN_Server(FormCollection formObj)
+        {
+            string JSON_TEXT = formObj["JSON_TEXT"];
+            if(System.IO.File.Exists(Server.MapPath("~/Files/JSON/MyScene.json")))
+            {
+                System.IO.File.Delete(Server.MapPath("~/Files/JSON/MyScene.json"));
+            }
+            else
+            {
+                System.IO.File.WriteAllText(Server.MapPath("~/Files/JSON/MyScene.json"), JSON_TEXT);
+            }
+           
+            
+            ViewBag.Succ = "succ to create file :)";
+            return View();
+        }
+        /*#######################################################
+         * load compelte scene in json  file
+         * #####################################################*/
+        public ActionResult LoadCompleteScene()
+        {
+            return View();
+        }
+
+        /*#######################################################
+        * load object in json file
+        * #####################################################*/
+        public ActionResult LoadObject()
+        {
+            return View();
+        }
+
+        /************************************************************************
+         * **********************************************************************
+         *  2. blender
+         *  **********************************************************************
+         *  ********************************************************************/
+        public ActionResult ImportObjectFromBlender()
+        {
+            return View();
+        }
+
+        public ActionResult ImportObjectFromBlender2()
+        {
+            return View();
+        }
+
+        public ActionResult ImportMultiObject_FromBlender()
+        {
+            return View();
+        }
+
+        /************************************************************************
+         * **********************************************************************
+         *    .obj and .mtl
+         *  **********************************************************************
+         *  ********************************************************************/
+        public ActionResult ImportDotObj()
+        {
+            return View();
+        }
+    }
+}
